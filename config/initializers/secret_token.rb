f37e5074c422c0ebc72cae91e51b36a68175eef1
RD99 <sakkiraju@hotmail.com>
2012-10-04 23:37:07 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogic::Application.config.secret_token = '99c52b264899a71f7ced1c1bd40d9acb931a361f29fefada56562f81bccf65d47536720bf516f165863f4761965893990e81f388ea255c055fd972e2e0a2e03f'
