9af10e9752f6985f1177a2058e9faf7ce0cb226c
JVB <sakkiraju@hotmail.com>
2012-10-04 23:37:07 -0700

    Initial commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Blogic::Application.config.session_store :cookie_store, key: '_blogic_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Blogic::Application.config.session_store :active_record_store
